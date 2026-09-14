f938b13528341ce62af5a881ecb7633de15bfd57
asistentepaul-ai <asistentepaul@gmail.com>
2026-09-14 20:26:29 +0200

    v5: nombre del ejercicio visible en pantalla de trabajo
    - work-name en grande junto al GIF y las reps durante la ejecucion
    - bump SW a v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fuerza10k-v3";
+const CACHE = "fuerza10k-v4";
 const PRECACHE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
