b961ed916d9e4959570fa510f3d177447bf44efa
Paul (OpenClaw) <openclaw@MacMini>
2026-09-15 09:11:58 +0200

    v6: restaurar timer de descanso (showRest tras cada serie) + invalidar cache SW

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fuerza10k-v4";
+const CACHE = "fuerza10k-v6";
 const PRECACHE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
