b0d8e772dece6ba7fab989335d337cf6049d6bad
Paul (OpenClaw) <openclaw@MacMini>
2026-09-15 15:06:20 +0200

    v9: botón sesión B igual que A + fondo beige claro menos verde

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fuerza10k-v8";
+const CACHE = "fuerza10k-v9";
 const PRECACHE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
